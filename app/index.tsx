97357b1ae242287c576a06dd82041be1085bcbc3
Keryan <championkeryan@gmail.com>
2025-10-29 11:41:09 +0100

    clean

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,9 @@
-import { Book } from "@/model/Book";
-import getBooks from "@/service/BookService";
 import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
 import { Pressable, Text, View } from "react-native";
 export default function Index() {
-  const [books, setBooks] = useState<Book[]>([]);
 
   const router = useRouter();
 
-  useEffect(() => {
-    getBooks().then((data) => setBooks(data));
-  }, []);
-  console.log(books);
   return (
     <View
       style={{
